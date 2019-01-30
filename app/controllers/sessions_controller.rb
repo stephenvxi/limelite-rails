0a7f2bd591bde95a045314d29b5b69e18d7a0022
stephenvxi <stephen.v.xi@gmail.com>
2019-01-30 21:02:02 +0000

    Auth system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,7 +26,8 @@ class SessionsController < ApplicationController
         session[:user_id] = user.id
         flash[:success] = "You have successfully logged in"
         format.html { redirect_to root_path }
-        format.json { render :json => user.to_json(:include => [:auth_token]) }
+        format.json { render :json => { :user => user
+                                        :auth_token: auth_token } }
       else
         flash[:danger] = "Incorrect email or password"
         format.html { render :new }
